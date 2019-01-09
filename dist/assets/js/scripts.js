39b007f28716bc2fb4c3e9d71f9fc5b969376ba5
Cung911 <Cung911@gmail.com>
2019-01-09 08:57:46 +0700

    Add New Homagepage

--- dist/assets/js/scripts.js
+++ dist/assets/js/scripts.js
@@ -4,12 +4,6 @@
 // window.sr = ScrollReveal({duration:1000, scale:0, origin:'bottom',viewport: document.getElementById('fullpage')});
 window.sr = new ScrollReveal();
 
-sr.reveal('.wazz-logo', {
-	delay:100,
-	opacity:  0,
-  scale: 0.8,
-	easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)',
-});
 
 sr.reveal('.coming-fucking-soon', {
 	delay:300,
